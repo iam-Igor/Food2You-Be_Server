bb78da738095627c52f84d1ada9f182bb9fcda46
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-27 11:53:10 +0100

    Initial commit

---
+++ src/main/java/ygorgarofalo/Food2YouBe_Server/Food2YouBeServerApplication.java
@@ -0,0 +1,13 @@
+package ygorgarofalo.Food2YouBe_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Food2YouBeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Food2YouBeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ygorgarofalo/Food2YouBe_Server/Food2YouBeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ygorgarofalo.Food2YouBe_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Food2YouBeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
